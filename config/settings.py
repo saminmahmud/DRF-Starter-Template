3f4af40af37825cc1cccda6271d9b96a4a94e3ef
saminmahmud <samin.mahmud18@gmail.com>
2026-05-15 16:47:19 +0600

    add django-filter

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
+    'django_filters',
     'rest_framework.authtoken',
     'allauth',
     'allauth.account',
@@ -182,6 +183,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
